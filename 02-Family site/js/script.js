cd91e4d58f76ede002d8ecb44c8bf0e2098a2e62
birds0501 <birds0501@naver.com>
2023-12-04 12:57:02 +0900

    디자인 커스텀 완성

--- 02-Family site/js/script.js
+++ 02-Family site/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  //대상를 변수에 저장
+  //대상을 변수에 저장
   const $selectMenu = $(".select-menu");
 
   //셀렉트 박스를 클릭하면 --> option의 선택을 바뀌면
@@ -13,5 +13,27 @@ $(function () {
 
     //새 창 열기 --> window.open()
     window.open(linkValue);
+  });
+
+  //2.디자인 커스텀
+  const $selectWrap = $(".select-wrap");
+  const $selectList = $(".select-list");
+
+  //strong(Family site를 클릭하면 $selectList를 보이게)
+  $selectWrap.find("strong").on("click", function () {
+    $selectList.slideToggle();
+  });
+
+  $selectList.children("li").on("click", function () {
+    //attr('속성명')
+    // const linkValue = $(this).attr("data-link"); //변수 유효 범위가 중괄호 안이기 때문에(지역변수) const 반복하여 쓸 수 있다.
+
+    //data('속성명')
+    const linkValue = $(this).data("link");
+    console.log(linkValue);
+
+    window.open(linkValue);
+
+    $selectList.slideUp();
   });
 });
